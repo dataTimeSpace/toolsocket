a8220abee99d6f394d2db8a9b7f9e3f1ab32568f
James Hobin <jhobin@datatime.space>
2025-05-27 16:25:25 -0400

    Add back rules to new eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,57 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig, globalIgnores } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
-  globalIgnores([
-    "dist/*",
-  ]),
-  {
-    files: ["**/*.{js,mjs,cjs}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    rules: {
-      'no-unused-vars': ['error', {
-        argsIgnorePattern: '^_',
-        varsIgnorePattern: '^_',
-        caughtErrorsIgnorePattern: '^_',
-      }],
+    globalIgnores([
+        'dist/*',
+    ]),
+    {
+        files: ['**/*.{js,mjs,cjs}'],
+        plugins: { js },
+        extends: ['js/recommended'],
+
+        rules: {
+            'indent': ['warn', 4],
+            'linebreak-style': ['error', 'unix'],
+            'quotes': 'off',
+            'semi': ['warn', 'always'],
+
+            'comma-spacing': ['warn', {
+                before: false,
+                after: true,
+            }],
+
+            'key-spacing': 'warn',
+            'keyword-spacing': 'warn',
+            'no-trailing-spaces': 'warn',
+
+            'brace-style': ['warn', '1tbs', {
+                allowSingleLine: true,
+            }],
+
+            'space-before-blocks': 'warn',
+            'space-infix-ops': 'warn',
+            'no-prototype-builtins': 'off',
+
+            'no-unused-vars': ['warn', {
+                argsIgnorePattern: '^_',
+                varsIgnorePattern: '^_',
+                caughtErrorsIgnorePattern: '^_',
+            }],
+
+            'no-redeclare': 'warn',
+            'no-inner-declarations': 'warn',
+            'no-extra-semi': 'warn',
+            'require-atomic-updates': 'off',
+            'max-statements-per-line': 'warn',
+        },
     },
-  },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+    { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+    {
+        files: ['**/*.{js,mjs,cjs}'],
+        languageOptions: {
+            globals: {...globals.browser, ...globals.node},
+        },
+    },
 ]);
